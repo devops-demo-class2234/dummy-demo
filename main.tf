8f602b30ac50b6bdc594b479f8fd2a94e52d1eb9
Ravi Mishra <inmishrar@gmail.com>
2023-05-04 07:31:29 +0530

    added main.tf

---
+++ main.tf
@@ -0,0 +1,21 @@
+terraform {
+  required_providers {
+    random = {
+      source = "hashicorp/random"
+      version = "3.5.1"
+    }
+  }
+}
+
+provider "random" {
+  # Configuration options
+}
+
+resource "random_password" "password" {
+  length           = 16
+  special          = true
+  override_special = "!#$%&*()-_=+[]{}<>:?"
+}
+output "password" {
+  value = random_password.password.result
+}
